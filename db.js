be764c4e81f3c450b9f4db7b2ed9ad461d0c0f9d
Ismael Cardenas Gomez <inficg8@gmail.com>
2023-11-20 18:16:29 +0000

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,1 @@
-const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: 'booking-391110:europe-west2:admin',
-  user: 'izzy',
-  password: 'D"$DYSQX6=fU@lEF',
-  database: 'bookingdb'
-});
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err);
-    return;
-  }
-  console.log('Connected to the database.');
-});
-
-module.exports = connection;
